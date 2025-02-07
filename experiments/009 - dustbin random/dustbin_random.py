47c12f69521398105191e49ee4f71b2ebeaf17c3
AntaoA <amoryantao@net-c.com>
2025-02-07 11:20:17 +0100

    update

--- experiments/009 - dustbin random/dustbin_random.py
+++ experiments/009 - dustbin random/dustbin_random.py
@@ -12,22 +12,25 @@ def capture_all_texts(text):
 
 batches_path = [f"data/dustbin__{i:02d}" for i in range(1, 23)]
 
-def select_random_genomes():
+def select_random_genomes(nb):
     genomes_list = []
     for batch in batches_path:
         genomes = os.listdir(batch)
         genomes = [f"{batch}/{genome}" for genome in genomes]
         genomes_list.extend(genomes)
     random.shuffle(genomes_list)
-    return genomes_list[:1000]
+    return genomes_list[:nb]
 
 path_attotree = "data/selected_genomes/"
 
 def del_genomes():
     for genome in os.listdir(path_attotree):
-        gen = genome.split("+")[0] +"/"+ genome.split("+")[1]
-        os.system(f"mv {path_attotree}{genome} data/{gen}")
-    
+        if genome.endswith(".fa"):
+            gen = genome.split("+")[0] +"/"+ genome.split("+")[1]
+            os.system(f"mv {path_attotree}{genome} data/{gen}")
+        else:
+            os.system(f"rm {path_attotree}{genome}")
+            
 def move_genomes(genomes):
     for genome in genomes:
         gen = genome.split("/")[1] +"+"+ genome.split("/")[2]
@@ -64,7 +67,7 @@ def run_command(command):
 
 
 if __name__ == "__main__":
-    # dustbin_random.py [type_of_order] [new_select]
+    # dustbin_random.py [type_of_order] [new_select] [nb]
     # type_of_order: r, l, p for random, lexicographic, phylogenetic
     # r by default
     # new_select: y, n for yes, no
@@ -74,8 +77,9 @@ if __name__ == "__main__":
         order = sys.argv[1]
         if len(sys.argv) > 2:
             new_select = sys.argv[2]
+            nb = int(sys.argv[3])
             if new_select == "y":
-                genomes = select_random_genomes()
+                genomes = select_random_genomes(nb)
                 del_genomes()
                 move_genomes(genomes)
             else:
